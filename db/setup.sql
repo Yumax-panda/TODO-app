a795be589e328527203485ad6e263993e62bc2d9
Yumax-panda <yuma0wall@gmail.com>
2023-07-27 13:36:43 +0900

    created table schema

---
+++ db/setup.sql
@@ -0,0 +1,43 @@
+set client_encoding = 'UTF8';
+
+CREATE TABLE IF NOT EXISTS users (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+)
+
+CREATE TABLE IF NOT EXISTS tasks (
+    "id" SERIAL PRIMARY KEY,
+    "title" VARCHAR(255) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "is_done" BOOLEAN NOT NULL DEFAULT FALSE,
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "expires_at" TIMESTAMP DEFAULT NOW(),
+    "completed_at" TIMESTAMP DEFAULT NOW(),
+    "priority" INTEGER NOT NULL DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+)
+
+CREATE TABLE IF NOT EXISTS tags (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "color" INTEGER NOT NULL DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+)
+
+CREATE TABLE IF NOT EXISTS relation (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER NOT NULL,
+    "task_id" INTEGER NOT NULL,
+    "tag_id" INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (task_id) REFERENCES tasks (id),
+    FOREIGN KEY (tag_id) REFERENCES tags (id)
+)
