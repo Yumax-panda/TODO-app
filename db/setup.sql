4001ab22a840805b936052976a76dc4ea5134af2
Yumax-panda <yuma0wall@gmail.com>
2023-08-01 22:49:29 +0900

    changed variable type

--- db/setup.sql
+++ db/setup.sql
@@ -1,18 +1,18 @@
 set client_encoding = 'UTF8';
 
 CREATE TABLE IF NOT EXISTS users (
-    "id" VARCHAR(255) NOT NULL PRIMARY KEY,
+    "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    UNIQUE("id")
+    UNIQUE(id)
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
     "id" SERIAL PRIMARY KEY,
     "title" VARCHAR(255) NOT NULL,
-    "user_id" VARCHAR(255) NOT NULL,
+    "user_id" INTEGER NOT NULL,
     "description" VARCHAR(255) NOT NULL,
     "is_done" BOOLEAN NOT NULL DEFAULT FALSE,
     "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS tasks (
     "expires_at" TIMESTAMP DEFAULT NOW(),
     "completed_at" TIMESTAMP DEFAULT NOW(),
     "priority" INTEGER NOT NULL DEFAULT 0,
+    UNIQUE(id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
@@ -27,17 +28,19 @@ CREATE TABLE IF NOT EXISTS tags (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
     "description" VARCHAR(255) NOT NULL,
-    "user_id" VARCHAR(255) NOT NULL,
+    "user_id" INTEGER NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
     "color" INTEGER NOT NULL DEFAULT 0,
+    UNIQUE(id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE IF NOT EXISTS relation (
     "id" SERIAL PRIMARY KEY,
-    "user_id" VARCHAR(255) NOT NULL,
+    "user_id" INTEGER NOT NULL,
     "task_id" INTEGER NOT NULL,
     "tag_id" INTEGER NOT NULL,
+    UNIQUE(id),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (task_id) REFERENCES tasks (id),
     FOREIGN KEY (tag_id) REFERENCES tags (id)
